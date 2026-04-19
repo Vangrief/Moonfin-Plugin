9532ac93569c0543d6c4353562af8dc81cfe4631
Axl Nunez <axltico12@gmail.com>
2026-04-19 16:00:54 -0400

    Bump version to 1.8.2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-VERSION="${1:-1.8.1.0}"
+VERSION="${1:-1.8.2.0}"
 TARGET_ABI="${2:-10.10.0}"
 BUILD_TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
--- frontend/src/plugin.js
+++ frontend/src/plugin.js
@@ -1,5 +1,5 @@
 const Plugin = {
-    version: '1.8.1',
+    version: '1.8.2',
     name: 'Moonfin Web Plugin',
     initialized: false,
     _initializing: false,
